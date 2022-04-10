cbce4521b18dbbc074a5da31c6e6e8c478e48c17
Gavin Esberger <8173463+guess-burger@users.noreply.github.com>
2022-04-10 20:48:34 +0100

    Adding a dev.clj

---
+++ dev/dev.clj
@@ -0,0 +1,27 @@
+(ns dev
+  (:require [org.httpkit.client :as http]
+            [clojure.string :as str]
+            [cheshire.core :as json]
+            [wavelength.wavelength :as wv]))
+
+;; telewave has the wavelengths in a js file
+(def ^:private wave-data "https://raw.githubusercontent.com/gjeuken/telewave/master/scripts/data.js")
+
+(defn generate-waves
+  []
+  (let [{:keys [status headers body error] :as resp} @(http/get wave-data)
+        body (str/replace body #"(var data = |;)" "")
+        data (json/parse-string body)]
+    (spit "resources/prompts.edn" (pr-str data))))
+
+
+(defn start []
+  (wv/-main))
+
+
+
+(comment
+
+  (generate-waves)
+
+  )
